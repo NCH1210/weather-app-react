0aa1f7d3ad51421e187041379d2be03dbb5714e3
Nina Haide <nina.haide@live.de>
2020-05-17 11:40:45 +0200

    fix

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import axios from "axios";
 
 export default function Search(props) {
   function handleSubmit(event) {
@@ -11,6 +12,18 @@ export default function Search(props) {
     props.setCity(event.target.value);
     Search();
   }
+  /* function searchPosition(position) {
+    props.setCity(position.target.value);
+    navigator.geolocation.getCurrentPosition(Search);
+  }
+
+  function searchLocation(position) {
+    let apiKey = "f45e74d7c2b9da42eac08c872edb3d3d";
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(searchPosition);
+  } */
 
   return (
     <div className="Search">
@@ -23,6 +36,7 @@ export default function Search(props) {
         />
         <input type="submit" className="btn-search" value="Search" />
         <input
+          /*onClick={searchLocation}*/
           type="button"
           className="btn-current"
           value="Current"
