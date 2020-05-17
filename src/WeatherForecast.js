4a66415c0894538216f800dbf77ae85da2144562
Nina Haide <nina.haide@live.de>
2020-05-17 10:41:52 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -22,27 +22,27 @@ export default function WeatherForecast(props) {
         </h4>
         <div className="container-group">
           <div className="row" id="forecast-block">
-            <div className="col" id="element">
+            <div className="col-2" id="element">
               {new Date(forecast.list[0].dt * 1000).getHours()}:00
               <WeatherIcon data={forecast.list[0].weather[0].icon} />
               {Math.round(forecast.list[0].main.temp)}°C
             </div>
-            <div className="col" id="element">
+            <div className="col-2" id="element">
               {new Date(forecast.list[1].dt * 1000).getHours()}:00
               <WeatherIcon data={forecast.list[1].weather[0].icon} />
               {Math.round(forecast.list[1].main.temp)}°C
             </div>
-            <div className="col" id="element">
+            <div className="col-2" id="element">
               {new Date(forecast.list[2].dt * 1000).getHours()}:00
               <WeatherIcon data={forecast.list[2].weather[0].icon} />
               {Math.round(forecast.list[2].main.temp)}°C
             </div>
-            <div className="col" id="element">
+            <div className="col-2" id="element">
               {new Date(forecast.list[3].dt * 1000).getHours()}:00
               <WeatherIcon data={forecast.list[3].weather[0].icon} />
               {Math.round(forecast.list[3].main.temp)}°C
             </div>
-            <div className="col" id="element">
+            <div className="col-2" id="element">
               {new Date(forecast.list[4].dt * 1000).getHours()}:00
               <WeatherIcon data={forecast.list[4].weather[0].icon} />
               {Math.round(forecast.list[4].main.temp)}°C
